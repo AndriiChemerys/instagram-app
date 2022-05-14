b9e3dcc764f30f4a5fb27db58bc14cba0e585a2f
AndriiChemerys <andriichemerys@gmail.com>
2022-05-14 08:36:00 +0200

    IG

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React, { useState } from "react";
 import "./App.css";
 import Post from "./Post";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "Sara",
+      imageAvatar: "img\fit_girl_1_b.jpg",
+      caption: "Chilling.. <3",
+      imageUrl: "img\fit_girl_1_a.jpg",
+    },
+    {
+      username: "Andrii",
+      imageAvatar: "imgchemerys_andrii_1.png",
+      caption: "Stand with UA",
+      imageUrl: "imgchemerys_andrii_2.jpg",
+    },
+    {
+      username: "Sensual",
+      imageAvatar: "img\fit_girl_2.png",
+      caption: "New home :)",
+      imageUrl: "imginterior_1.jpg",
+    },
+  ]);
+
   return (
     <div className="App">
       <div className="app__header">
@@ -12,7 +34,16 @@ function App() {
         />
       </div>
 
-      <Post
+      {posts.map((post) => (
+        <Post
+          username={post.username}
+          imageAvatar={post.imageAvatar}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
+
+      {/* <Post
         username="Sara"
         imageAvatar="\img\fit_girl_1_b.jpg"
         caption="Chilling.. <3"
@@ -29,7 +60,7 @@ function App() {
         imageAvatar="\img\fit_girl_2.png"
         caption="New home :)"
         imageUrl="\img\interior_1.jpg"
-      />
+      /> */}
     </div>
   );
 }
